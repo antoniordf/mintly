cebdd49b8b90e7589c4c72c93479f593f4e5581b
Gaoren Hao <gaorenh@gmail.com>
2022-11-28 18:04:05 +0000

    finished routes for nfts

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :portfolios
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :collections do
+    resources :nfts, only: [:new, :create]
+  end
+  resources :nfts, only: [:index, :show, :edit, :update, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
