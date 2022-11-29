d598d8bcbb0749b7b9317247fbcb3ef6d6104e93
Gaoren Hao <gaorenh@gmail.com>
2022-11-29 14:22:56 +0000

    APIyeah

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "json"
+require "rest-client"
+response = RestClient.get"https://api.rarify.tech/data/contracts/ethereum:0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D", {:Authorization => 'Bearer 6d42ff96-f7b6-4abd-8c87-b097789b71d5'}
+repos = JSON.parse(response)
+# => repos is an `Array` of `Hashes`.
+
+# repos.size
+p repos
+collection1 = Collection.new(
+  description:
+  name:
+  
+)
+
+collection1.save!
