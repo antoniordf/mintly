18a3932a8ecee206d0e4d15ac93327906b661e6f
Gaoren Hao <gaorenh@gmail.com>
2022-11-30 11:37:26 +0000

    edited comment in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,32 +13,28 @@ metadata = JSON.parse(url_metadata)
 url_price_history = RestClient.get"https://api.rarify.tech/data/contracts/ethereum:0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D/insights/24h", {:Authorization => 'Bearer 6d42ff96-f7b6-4abd-8c87-b097789b71d5'}
 # url_price_history = RestClient.get"https://api.rarify.tech/data/contracts/ethereum:0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D/insights/7d", {:Authorization => 'Bearer 6d42ff96-f7b6-4abd-8c87-b097789b71d5'} # 7days data for array
 price_history = JSON.parse(url_price_history)
-p price_history
-
+#p price_history
 # p metadata
 
 
-user = User.new(
-  email: "haogaoren1se111dd198@gmail.com",
+user = User.create(
+  email: "haogaoren1@gmail.com",
   password: "123456"
 )
-user.save!
 
-bored_ape_yacht_club = Collection.new(
+bored_ape_yacht_club = Collection.create(
   name: metadata["data"]["attributes"]["name"],
   description: metadata["data"]["attributes"]["description"],
   link: metadata["data"]["attributes"]["external_url"],
   image: "https://i.seadn.io/gae/i5dYZRkVCUK97bfprQ3WXyrT9BnLSZtVKGJlKQ919uaUB0sxbngVCioaiyu9r6snqfi2aaTyIvv6DHm4m2R3y7hMajbsv14pSZK8mhs?auto=format&w=2048",
   portfolio_id: user.portfolio.id
 )
-bored_ape_yacht_club.save!
 p bored_ape_yacht_club
 
-bored_ape_yacht_club_hitory = History.new(
+bored_ape_yacht_club_hitory = History.create(
   price: price_history["included"][0]["attributes"]["min_price"].to_f/1000000000000000000, ##24h price as a float
-  # date_time: price_history["included"][1]["attributes"]["history"].map { |day| day["time"] },  ##as an array (only work if change api addresses 24h to 7d)
-  # date_time: price_history["included"][1]["attributes"]["history"].map { |price| price["time"] }, ##as an array (only work if change api addresses 24h to 7d)
+  # date_time: price_history["included"][1]["attributes"]["history"].map { |day| day["time"] },  ##as an array (only work if change api end point from 24h to 7d)
+  # price: price_history["included"][1]["attributes"]["history"].map { |price| price["min_price"] }, ##as an array (only work if change api end point from 24h to 7d)
   collection_id: bored_ape_yacht_club.id
 )
-bored_ape_yacht_club_hitory.save!
 p bored_ape_yacht_club_hitory
