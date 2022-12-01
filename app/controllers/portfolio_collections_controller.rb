b3b5fe98565610dbd11ed4f1463eb210d2263f0d
Gaoren Hao <gaorenh@gmail.com>
2022-12-01 17:02:12 +0000

    able to avoid duplicates in database when search & started on portfolio_collection controller and updated routes

---
+++ app/controllers/portfolio_collections_controller.rb
@@ -0,0 +1,8 @@
+class PortfolioCollectionsController < ApplicationController
+  def create
+    # target the collection
+    # get user using current user
+    # create new instance of portfolio controller using the collection and the user
+    # redirect back to the collection show page
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :collections do
     resources :nfts
+    resources :portfolio_collections, only: [:create]
   end
   resources :portfolios, only: [:index]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
