e99adaba566b8771703df9e3d091186a314febcc
Gaoren Hao <111617079+GaorenHao@users.noreply.github.com>
2022-12-07 15:07:35 +0000

    Update 20221128164630_create_nfts.rb

--- db/migrate/20221128164630_create_nfts.rb
+++ db/migrate/20221128164630_create_nfts.rb
@@ -1,7 +1,7 @@
 class CreateNfts < ActiveRecord::Migration[7.0]
   def change
     create_table :nfts do |t|
-      t.integer :name
+      t.string :name
       t.date :purchase_date
       t.float :purchase_price
       t.references :collection, null: false, foreign_key: true
